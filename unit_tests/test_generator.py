4b709dfa921145053c0856aefce6e8662df23963
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2020-11-27 09:24:07 +0700

    add test pixshuffle

--- unit_tests/test_generator.py
+++ unit_tests/test_generator.py
@@ -12,6 +12,14 @@ class TestGenerator(unittest.TestCase):
 		res_blk = Residual_Block(in_channels=C_in, out_channels=C_out, strides=1)
 		self.assertEqual(res_blk(X).shape, (N, C_out, H, W))
 
+	def test_pixel_shuffle_blk(self):
+		N, C, H, W = 7, 256, 40, 40
+		upscale_factor = 2
+		X = torch.rand(size=(N, C, H, W ))
+		ps = PixelShufflerBlock(in_channels=C, upscale_factor=upscale_factor)
+		out = ps(X)
+		self.assertEqual(out.shape, (N, C/upscale_factor**2, H*upscale_factor, W*upscale_factor))
+
 if __name__ == '__main__':
 	unittest.main()
 
