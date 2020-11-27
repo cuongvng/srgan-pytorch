a3bf3a2780efe8ca559db28afe2a7b479732df64
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2020-11-27 15:58:34 +0700

    add xavier init

---
+++ src/train.py
@@ -0,0 +1,16 @@
+import torch
+import torch.optim as optim
+import torchsummary
+from torch.utils.data import DataLoader
+from torchvision.transforms import CenterCrop
+import sys
+sys.path.append("../")
+from data_loader import DIV2K
+from CONFIG import LR_CROPPED_SIZE, HR_CROPPED_SIZE
+from generator import Generator
+from discriminator import Discriminator
+
+
+def xavier_init_weights(model):
+	if isinstance(model, torch.nn.Linear) or isinstance(model, torch.nn.Conv2d):
+		torch.nn.init.xavier_uniform_(model.weight)
