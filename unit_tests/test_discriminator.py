e141a38850adb1cb5514ddfee34728a9052ceb5b
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2020-11-27 10:48:59 +0700

    test D

--- unit_tests/test_discriminator.py
+++ unit_tests/test_discriminator.py
@@ -2,18 +2,28 @@ import unittest
 import sys
 sys.path.append("../")
 import torch
+import math
 from src.discriminator import Discriminator, ConvBlock
 
 class TestDiscriminator(unittest.TestCase):
 	def test_conv_blk(self):
-		N, C_in, H, W = 2, 16, 100, 100
+		N, C_in, H, W = 2, 16, 25, 25
 		C_out = 2*C_in
-		X = torch.randn(size=(N, C_in, H, W))
+		X = torch.randn(size=(N, C_in, H, W), requires_grad=False)
 		conv1 = ConvBlock(in_channels=C_in, out_channels=C_in, strides=1)
 		conv2 = ConvBlock(in_channels=C_in, out_channels=C_out, strides=2)
-		out1, out2 = conv1(X), conv2(X)
+		with torch.no_grad():
+			out1, out2 = conv1(X), conv2(X)
 		self.assertEqual(out1.shape, (N, C_in, H, W))
-		self.assertEqual(out2.shape, (N, C_out, H//2, W//2))
+		self.assertEqual(out2.shape, (N, C_out, math.ceil(H/2), math.ceil(W/2)))
+
+	def test_D(self):
+		N, C_in, H, W = 2, 3, 223, 169
+		X = torch.randn(size=(N, C_in, H, W), requires_grad=False)
+		d = Discriminator(H, W)
+		with torch.no_grad():
+			out = d(X)
+		self.assertEqual(out.shape, (N, 1))
 
 if __name__ == '__main__':
 	unittest.main()
