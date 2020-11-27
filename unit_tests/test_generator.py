8a5b7d2b4586201368721ce7ed8508e26bb6ef46
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2020-11-27 09:23:44 +0700

    clean up

--- unit_tests/test_generator.py
+++ unit_tests/test_generator.py
@@ -2,13 +2,15 @@ import unittest
 import sys
 import torch
 sys.path.append("../")
-from src.gan import Residual_Block, PixelShuffler, Generator
+from src.gan import Residual_Block, PixelShufflerBlock, Generator
 
 class TestGenerator(unittest.TestCase):
 	def test_res_blk(self):
-		X = torch.randn(size=(2, 3, 100, 100))
-		res_blk = Residual_Block(in_channels=3, out_channels=16, strides=1)
-		self.assertEqual(res_blk(X).shape, (2, 16, 100, 100))
+		N, C_in, H, W = 2, 3, 100, 100
+		C_out = 16
+		X = torch.randn(size=(N, C_in, H, W))
+		res_blk = Residual_Block(in_channels=C_in, out_channels=C_out, strides=1)
+		self.assertEqual(res_blk(X).shape, (N, C_out, H, W))
 
 if __name__ == '__main__':
 	unittest.main()
