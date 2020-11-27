0fb2f30329553ac57039e323c0f1395a6cb03d97
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2020-11-27 15:17:28 +0700

    test dataloader ok

---
+++ unit_tests/test_dataloader.py
@@ -0,0 +1,31 @@
+import unittest
+import sys
+sys.path.append("../")
+import torchvision.transforms as trf
+from src.data_loader import DIV2K
+from CONFIG import LR_CROPPED_SIZE, HR_CROPPED_SIZE
+
+class TestDataLoader(unittest.TestCase):
+	def test_train_lr(self):
+		data = DIV2K(data_dir="../DIV2K/DIV2K_train_LR_bicubic", transform=trf.CenterCrop(size=LR_CROPPED_SIZE))
+		for i in range(len(data)):
+			self.assertEqual(data[i].shape, (3, LR_CROPPED_SIZE, LR_CROPPED_SIZE))
+
+	def test_train_hr(self):
+		data = DIV2K(data_dir="../DIV2K/DIV2K_train_HR", transform=trf.CenterCrop(size=HR_CROPPED_SIZE))
+		for i in range(len(data)):
+			self.assertEqual(data[i].shape, (3, HR_CROPPED_SIZE, HR_CROPPED_SIZE))
+
+	def test_valid_lr(self):
+		data = DIV2K(data_dir="../DIV2K/DIV2K_valid_LR_bicubic", transform=trf.CenterCrop(size=LR_CROPPED_SIZE))
+		for i in range(len(data)):
+			self.assertEqual(data[i].shape, (3, LR_CROPPED_SIZE, LR_CROPPED_SIZE))
+
+	def test_valid_hr(self):
+		data = DIV2K(data_dir="../DIV2K/DIV2K_valid_HR", transform=trf.CenterCrop(size=HR_CROPPED_SIZE))
+		for i in range(len(data)):
+			self.assertEqual(data[i].shape, (3, HR_CROPPED_SIZE, HR_CROPPED_SIZE))
+
+
+if __name__ == '__main__':
+	unittest.main()
