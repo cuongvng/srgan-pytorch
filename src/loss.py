c38ecc141cb91360809fae5c799015d106f9a739
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2020-12-03 15:07:19 +0700

    add perceptual loss

---
+++ src/loss.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+from torchvision.models import vgg19
+
+class PerceptualLoss(nn.Module):
+	def __init__(self, vgg_coef, adversarial_coef):
+		super(PerceptualLoss, self).__init__()
+		_vgg19 = vgg19(pretrained=True)
+		self.vgg19 = nn.Sequential(*_vgg19.features).eval()
+		for p in self.vgg19.parameters():
+			p.requires_grad = False
+		self.euclidean_distance = nn.MSELoss()
+		self.vgg_coef = vgg_coef
+		self.adversarial_coef = adversarial_coef
+
+	def forward(self, sr_img, hr_img, output_labels):
+		adversarial_loss = torch.mean(1-output_labels)
+		vgg_loss = self.euclidean_distance(self.vgg19(sr_img), self.vgg19(hr_img))
+		pixel_loss = self.euclidean_distance(sr_img, hr_img)
+		return pixel_loss + self.adversarial_coef*adversarial_loss + self.vgg_coef*vgg_loss
